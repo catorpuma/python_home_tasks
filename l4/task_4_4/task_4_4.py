6889d07edf1381adcae9b217c5abe2d9a6f2ec83
catorpuma <110312470+catorpuma@users.noreply.github.com>
2022-08-29 00:44:27 +0300

    please review

---
+++ l4/task_4_4/task_4_4.py
@@ -0,0 +1,9 @@
+# task_4_4
+import utils
+
+
+print(utils.currency_rates('USD'))
+print(utils.currency_rates('AUD'))
+print(utils.currency_rates('JPY'))
+print(utils.currency_rates('EUR'))
+print(utils.currency_rates(4))
